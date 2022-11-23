80eeb2a6652b3902fbb66b9b7cd456d84fca96a9
KangQinHao <113825020+KangQinHao@users.noreply.github.com>
2022-11-23 21:02:06 +0800

    Update homework06.js

--- homework/homework06/homework06.js
+++ homework/homework06/homework06.js
@@ -10,9 +10,7 @@ window.onload = function() {
         aa = data.value;    // 获取input标签里的值
 
         // 业务逻辑
-        if (aa == 0) {
-            reward = 0;
-        } else if (aa > 0 && aa <= 100000) {   // 第一种情况
+        if (aa >= 0 && aa <= 100000) {   // 第一种情况
             reward = aa * 0.1;
         } else if (aa >= 100000 && aa < 200000) {   // 第二种情况
             reward = (100000 * 0.1) + ((aa - 100000) * 0.075);
@@ -22,24 +20,23 @@ window.onload = function() {
             reward = (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + ((aa - 400000) * 0.03);
         } else if (aa >= 600000 && aa < 1000000) {  // 第五种情况
             reward = (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + (200000 * 0.03) + ((aa - 600000) * 0.015);
-        } else if (aa > 1000000) {      // 最后一种情况
+        } else if (aa > 1000000) {      // 第六种情况
             reward = (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + (200000 * 0.03) + (400000 * 0.015) + ((aa - 600000) * 0.01);
-        } else {
+        } else {    // 非法输入
             reward = "输入错误"
         }
 
         console.log(reward);    // 在控制台输出
 
-        // 四舍五入保留两位小数
-//         if (!isNaN(reward)) {
-//             result.innerText = reward.toFixed(2);     // 输入合法，替换页面中的结果值
-//         }
-//         else {
-//             result.innerText = reward;  // 当输入非法时，显示错误信息
-//         }
-        
-        // 不四舍五入，直接输出
-        result.innerText = reward;
+        if (isNaN(reward)) {
+            result.innerHTML = reward;  // 当输入非法时，显示错误信息
+        } else {
+            // 不四舍五入
+            result.innerText = reward + "元";
+
+            // 四舍五入
+            // result.innerHTML = reward.toFixed(2) + "元";     // 输入合法，替换页面中的结果值
+        }
     }
 
     setInterval(count, 500);    // 设定自动执行
