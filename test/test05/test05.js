23f56d8d9403ff7a8addc6ec2e98b735e3cd3f3f
KangQinHao <113825020+KangQinHao@users.noreply.github.com>
2022-12-15 20:23:24 +0800

    Update test05.js

--- test/test05/test05.js
+++ test/test05/test05.js
@@ -1,17 +1,20 @@
 window.onload = function() {
-    var flag = false;
-    var myCanvas = document.getElementById("myCanvas");
-    var ctx = myCanvas.getContext("2d");
-    var sampleImage = document.getElementById("heart");
+  var flag = false;
+  var myCanvas = document.getElementById("myCanvas");
+  var ctx = myCanvas.getContext("2d");
+  var sampleImage = document.getElementById("heart");
 
-    function controlCanvas() {
-        if (flag) {
-            ctx.clearRect(0, 0, 250, 250);
-        } else {
-            ctx.drawImage(sampleImage, 0, 0, 250, 250);
-        }
-        flag = !flag;
-    }
+  function controlCanvas() {
+      ctx.drawImage(sampleImage, 25, 25, 200, 200);
+      if (flag) {
+          ctx.clearRect(0, 0, 250, 250);
+          ctx.drawImage(sampleImage, 25, 25, 200, 200);
+      } else {
+          ctx.clearRect(25, 25, 200, 200);
+          ctx.drawImage(sampleImage, 0, 0, 250, 250);
+      }
+      flag = !flag;
+  }
 
-    setInterval(controlCanvas, 750);
+  setInterval(controlCanvas, 750);
 }
